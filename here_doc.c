a35b332cf014898cefffa4004c3c013e3a22e924
dsa-mora <dsa-mora@student.42.fr>
2023-05-16 17:05:37 +0100

    WORKINGGGGGGGGG

--- here_doc.c
+++ here_doc.c
@@ -21,17 +21,17 @@ int	ft_here_doc(char *str)
 		perror("");
 	write(0, ">", 1);
 	in = get_next_line(0);
-	write(fd[1], in, ft_strlen(in));
 	while (1)
 	{
+		write(0, ">", 1);
 		if ((ft_strncmp(in, str, ft_strlen(str)) == 0) && \
 			(ft_strlen(in) - 1 == ft_strlen(str)))
 			break ;
-		write(0, ">", 1);
 		write(fd[1], in, ft_strlen(in));
 		free(in);
 		in = get_next_line(0);
 	}
 	free(in);
+	close(fd[1]);
 	return (fd[0]);
 }
