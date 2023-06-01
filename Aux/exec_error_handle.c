39d8eee90621aa979156a1c7b328d205c8d7c5ea
Manuel Teodoro <manuelteodoro@MBP-de-Manuel-3.lan>
2023-06-01 12:50:52 +0100

    is a direcotry error added,  echo hi fixed, not having a space before pipe fixed

--- Aux/exec_error_handle.c
+++ Aux/exec_error_handle.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_error_handle.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsa-mora <dsa-mora@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mtiago-s <mtiago-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 17:51:24 by dsa-mora          #+#    #+#             */
-/*   Updated: 2023/05/31 18:04:40 by dsa-mora         ###   ########.fr       */
+/*   Updated: 2023/06/01 12:36:55 by mtiago-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,13 @@ void	error_handle(char *cmd)
 	if (!cmd[i] || !cmd)
 	{
 		write(2, "Permission denied\n", 18);
+		return ;
+	}
+	else if (access(cmd, X_OK) == 0)
+	{
+		write(2, cmd, ft_strlen(cmd));
+		write(2, ": is a directory\n", 17);
+		g_data.status = 126;
 		return ;
 	}
 	else
